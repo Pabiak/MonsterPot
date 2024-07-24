77e5dd47751c3308ddcf31b0772b1de5b849850c
pabiak <pabiak17@gmail.com>
2024-07-25 00:49:29 +0200

    PT-24 add history page

--- src/pages/HistoryPage/HistoryPage.tsx
+++ src/pages/HistoryPage/HistoryPage.tsx
@@ -1,10 +1,17 @@
 import HistoryTile from '@/components/HistoryTile/HistoryTile';
 import HISTORY_TILE_TYPE from '@/types/enums/components/HistoryTypes';
 
+import './HistoryPage.scss';
+
 const HistoryPage = () => (
-  <div style={{ padding: '0.75rem' }}>
-    <HistoryTile type={HISTORY_TILE_TYPE.WATERING} title="Nawadnianie" subtext="Zakończone sukcesem" date="29 marca, 2024" />
-    <HistoryTile type={HISTORY_TILE_TYPE.ERROR} title="Za wysoka temperatura" subtext="Powiadomienie wysłane" date="29 marca, 2024" />
+  <div className="history-page">
+    <span className="history-page__title">History</span>
+    <div className="history-page__container">
+      <HistoryTile type={HISTORY_TILE_TYPE.WATERING} title="Nawadnianie" subtext="Zakończone sukcesem" date="24 marca, 2024" />
+      <HistoryTile type={HISTORY_TILE_TYPE.ERROR} title="Za wysoka temperatura" subtext="Powiadomienie wysłane" date="24 marca, 2024" />
+      <HistoryTile type={HISTORY_TILE_TYPE.WATERING} title="Nawadnianie" subtext="Zakończone sukcesem" date="24 marca, 2024" />
+      <HistoryTile type={HISTORY_TILE_TYPE.ERROR} title="Niski poziom wody" subtext="Powiadomienie wysłane" date="24 marca, 2024" />
+    </div>
   </div>
 );
 
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,27 +1,30 @@
-import Parameter from '@/components/Parameter/Parameter';
-import './HomePage.scss';
 import PARAMETER_TYPES from '@/types/enums/components/ParameterTypes';
+
+import Parameter from '@/components/Parameter/Parameter';
 import Button from '@/components/Button/Button';
+
 import FlowerImage from '@/assets/flower.png';
 
+import './HomePage.scss';
+
 const HomePage = () => (
-  <div className="homepage">
-    <span className="homepage__title">Homepage</span>
-    <div className="homepage__flower-info-container">
-      <h1 className="homepage__flower-name">Monstera</h1>
-      <span className="homepage__last-watering">Ostatnie podlewanie: 12:36</span>
+  <div className="home-page">
+    <span className="home-page__title">Homepage</span>
+    <div className="home-page__flower-info-container">
+      <h1 className="home-page__flower-name">Monstera</h1>
+      <span className="home-page__last-watering">Ostatnie podlewanie: 12:36</span>
     </div>
-    <div className="homepage__card">
-      <img src={FlowerImage} alt="flower" className="homepage__card__flower" />
-      <div className="homepage__card__parameters">
+    <div className="home-page__card">
+      <img src={FlowerImage} alt="flower" className="home-page__card__flower" />
+      <div className="home-page__card__parameters">
         <Parameter type={PARAMETER_TYPES.HUMIDITY} value="75%" />
         <Parameter type={PARAMETER_TYPES.LIGHT} value="84%" />
         <Parameter type={PARAMETER_TYPES.TEMPERATURE} value="17C" />
       </div>
-      <div className="homepage__card__last-update">
+      <div className="home-page__card__last-update">
         Ostatnia aktualizacja: 29.05.2024 10:45
       </div>
-      <div className="homepage__card__button">
+      <div className="home-page__card__button">
         <Button text="Pobierz aktualne dane" onClick={() => console.log('Aktualne dane')} />
       </div>
     </div>
