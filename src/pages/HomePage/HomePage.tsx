c6addfc1441afc6de4b25ca33e6b02c19db7bf39
pabiak <pabiak17@gmail.com>
2024-07-24 23:49:49 +0200

    PT-23 add homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,30 @@
-import Button from '@/components/Button/Button';
 import Parameter from '@/components/Parameter/Parameter';
+import './HomePage.scss';
 import PARAMETER_TYPES from '@/types/enums/components/ParameterTypes';
+import Button from '@/components/Button/Button';
+import FlowerImage from '@/assets/flower.png';
 
 const HomePage = () => (
-  <div style={{
-    display: 'flex', gap: '1rem', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh',
-  }}
-  >
-    <Button text="Pobierz aktualne dane" onClick={() => console.log('Button clicked')} />
-    <Button text="Zatwierdź" onClick={() => console.log('Button clicked')} />
-
-    <Parameter type={PARAMETER_TYPES.HUMIDITY} value="50%" />
-    <Parameter type={PARAMETER_TYPES.LIGHT} value="50%" />
-    <Parameter type={PARAMETER_TYPES.TEMPERATURE} value="50%" />
+  <div className="homepage">
+    <span className="homepage__title">Homepage</span>
+    <div className="homepage__flower-info-container">
+      <h1 className="homepage__flower-name">Monstera</h1>
+      <span className="homepage__last-watering">Ostatnie podlewanie: 12:36</span>
+    </div>
+    <div className="homepage__card">
+    <img src={FlowerImage} alt="flower" className="homepage__card__flower" />
+      <div className="homepage__card__parameters">
+        <Parameter type={PARAMETER_TYPES.HUMIDITY} value="75%" />
+        <Parameter type={PARAMETER_TYPES.LIGHT} value="84%" />
+        <Parameter type={PARAMETER_TYPES.TEMPERATURE} value="17C" />
+      </div>
+      <div className="homepage__card__last-update">
+        Ostatnia aktualizacja: 29.05.2024 10:45
+      </div>
+      <div className="homepage__card__button">
+        <Button text="Pobierz aktualne dane" onClick={() => console.log('Aktualne dane')} />
+      </div>
+    </div>
   </div>
 );
 
