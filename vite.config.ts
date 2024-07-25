422a6ea06ef83df322cb30f3c90b5926daf57062
pabiak <pabiak17@gmail.com>
2024-07-25 16:47:53 +0200

    PT-25 add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,54 @@
 import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), ['VITE_']);
 
   return {
-    plugins: [react(), svgr()],
+    plugins: [
+      react(),
+      svgr(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Pottery4.0',
+          short_name: 'Pottery',
+          description: 'Praca Magisterska - Pottery4.0',
+          "start_url": "/",
+          display: "standalone",
+          background_color: "#50B57A",
+          theme_color: "#50B57A",
+          icons: [
+            {
+              "src": "/pwa-192x192.png",
+              "sizes": "192x192",
+              "type": "image/png",
+              "purpose": "any"
+            },
+            {
+              "src": "/pwa-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png",
+              "purpose": "any"
+            },
+            {
+              "src": "/pwa-maskable-192x192.png",
+              "sizes": "192x192",
+              "type": "image/png",
+              "purpose": "maskable"
+            },
+            {
+              "src": "/pwa-maskable-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png",
+              "purpose": "maskable"
+            }
+          ],
+        },
+      }),
+    ],
     server: {
       port: 3000,
       open: true,
