1a4d9cb6dc3e9cf366e490fd983e8906a23b0961
pabiak <pabiak17@gmail.com>
2024-07-25 22:05:28 +0200

    PT-7 add statistics page

--- src/pages/StatisticsPage/StatisticsPage.tsx
+++ src/pages/StatisticsPage/StatisticsPage.tsx
@@ -1,13 +1,32 @@
-import StatisticsTile from "@/components/StatisticsTile/StatisticsTile";
-import PARAMETER_TYPES from "@/types/enums/components/ParameterTypes";
+import { useTranslation } from 'react-i18next';
 
-import "./StatisticsPage.scss";
+import PARAMETER_TYPES from '@/types/enums/components/ParameterTypes';
+import HISTORY_TILE_TYPE from '@/types/enums/components/HistoryTypes';
 
-const StatisticsPage = () => (
-  <div className="statistics-page">
-    <StatisticsTile type={PARAMETER_TYPES.HUMIDITY} value={122} />
-    <StatisticsTile type={PARAMETER_TYPES.TEMPERATURE} value={22} />
-  </div>
-);
+import HistoryTile from '@/components/HistoryTile/HistoryTile';
+import StatisticsTile from '@/components/StatisticsTile/StatisticsTile';
+
+import './StatisticsPage.scss';
+
+const StatisticsPage = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="statistics-page">
+      <span className="statistics-page__title">{t('statisticsPage.title')}</span>
+      <div className="statistics-page__statistics">
+        <div className="statistics-page__statistics__tiles">
+          <StatisticsTile type={PARAMETER_TYPES.HUMIDITY} value={122} />
+          <StatisticsTile type={PARAMETER_TYPES.TEMPERATURE} value={17} />
+        </div>
+        <HistoryTile type={HISTORY_TILE_TYPE.WATERING} title={t('statisticsPage.lastWatering')} subtext={t('common.notificationSent')} time="12:00" date="12.12.2021" />
+      </div>
+      <div className="statistics-page__card">
+        <span className="statistics-page__card__subtitle">
+          {t('statisticsPage.hydration')}
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default StatisticsPage;
